b210d06db9db668c3f0b0939a9e33cebbf47ee38
Tim Lindborg <applebeeit@proton.me>
2023-08-24 22:43:04 +0200

    newnav

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import './App.css';
 import React from 'react';
 import './components/css/NavigationMenu.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
--- src/components/NavigationMenu.tsx
+++ src/components/NavigationMenu.tsx
@@ -2,22 +2,23 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import './css/NavigationMenu.css';
 
+
 const NavigationMenu: React.FC = () => {
 return (
    
     <nav className="navbar">
       <ul className="navbar-list">
         <li className="navbar-item">
-        <Link to="/home">Home</Link>
+        <Link to="/home"><span className="icofont">&#xef47;</span> Home</Link>
         </li>
         <li className="navbar-item">
-        <Link to="/aboutme">About Me</Link>
+        <Link to="/aboutme"><span className="icofont">&#xed52;</span> About Me</Link>
         </li>
         <li className="navbar-item">
-        <Link to="/contact">My Projects</Link>
+        <Link to="/contact"><span className="icofont">&#xe967;</span> My Projects</Link>
         </li>
         <li className="navbar-item">
-        <Link to="/portfolio">Contact</Link>
+        <Link to="/portfolio"><span className="icofont">&#xec50;</span> Contact</Link>
         </li>
       </ul>
 
