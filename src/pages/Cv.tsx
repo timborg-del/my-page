455d55073fbab72c060937a679c7358858b80e74
timborg-del <timl@live.se>
2023-09-15 06:54:03 +0200

    cvupdate

--- src/pages/Cv.tsx
+++ src/pages/Cv.tsx
@@ -1,43 +1,57 @@
 import React from 'react';
 import { useLanguageContext } from '../components/LanguageProvider';
- 
+import lexicon from '../pages/img/logo-lexicon.jpg'; // Replace with the actual path to your first image
+import volvo from '../pages/img/volvo.png'; // Replace with the actual path to your second image
+
 export const Cv = () => {
   const { language } = useLanguageContext();
 
   return (
     <div>
       <header>
-        <h1 className='hline'>{language === 'swedish' ? 'CV' : 'CV'}
-        </h1>
+        <h1 className='hline'>{language === 'swedish' ? 'CV' : 'CV'}</h1>
       </header>
-      <div className="about-me-container">
-            <p className='slide-me'>
-        <strong className='headline-page'>
-          {language === 'swedish' ? 'Yrkeserfarenhet' : 'Professional Experience'}
-        </strong>
-        <br />
-        <span className="fade-in-fast">
-        <br />
-          <br />
+      <div className="cv-container">
+        <div className="text-cv">
+          <div className="image-placeholder">
+            <img src={volvo} alt="volvo Logo" className='img-class' />
+          </div>
           <strong className="slide-from-right fade-in-1">
             {language === 'swedish' ? 'Volvo Construction Equipment' : 'Volvo Construction Equipment'}
           </strong>
           <br />
-
           <em>{language === 'swedish' ? 'Korgsvetsare' : 'Basket Welder'}</em>
           {language === 'swedish' ? ' 2021 - Nutid' : ' 2021 - Present'}
           <br />
           {language === 'swedish' ? 'MAG-svetsningsteknik för tunga maskinkomponenter.' : 'Expertise in MAG welding technology for heavy machinery components.'}
           <br />
+        </div>
+        <div className="text-cv1">
+          <div className="image-placeholder">
+            <img src={lexicon} alt="lexicon" className="img-class" />
+          </div>
           <strong className="slide-from-right fade-in-2">
-            <br />
-            {language === 'swedish' ? 'Melagos Corner' : 'Melagos Corner'}
-         </strong><br />
+            {language === 'swedish' ? 'Melagos Corner med Lexicon' : 'Melagos Corner With Lexicon'}
+          </strong>
+          <br />
           <em>{language === 'swedish' ? 'Praktik' : 'Internship'}</em>
-          {language === 'swedish' ? ' 2021' : ' 2021'}
+          {language === 'swedish' ? ' 2020' : ' 2020'}
           <br />
           {language === 'swedish' ? 'Utvecklade ett försäljningssystem med användning av MySQL, MVC-arkitektur och React.' : 'Developed a sales system using MySQL, MVC architecture, and React.'}
           <br />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+
+
+
+
+
+          /* <br />
           <br />
           <strong className="slide-from-right fade-in-3">
           {language === 'swedish' ? 'Volvo Construction Equipment, Braås' : 'Volvo Construction Equipment, Braås'}
@@ -57,14 +71,14 @@ export const Cv = () => {
           {language === 'swedish' ? ' 2012 - 2016' : ' 2012 - 2016'}
           <br />
           {language === 'swedish' ? 'Lyckades framgångsrikt förvalta poolpumpavdelningen.' : 'Successfully managed the pool pump department.'}
-          <br />
+          // <br /> */
           {/* ... Rest of your content ... */}
-        </span>
-      </p>
+      //   </span>
+      // </p>
 
       {/* Button to toggle the language */}
-      </div>
-    </div>
-  );
-};
-export default Cv;
+//       </div>
+//     </div>
+//   );
+// };
+ export default Cv;
