6aa6f6da762d8e3c6dcc29600e6d5cca6748dccc
timborg-del <timl@live.se>
2023-09-16 13:12:56 +0200

    finisht

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -5,63 +5,103 @@ const AboutMe = () => {
   // Use the language and toggleLanguage function from the context
   const { language } = useLanguageContext();
 
+  const aboutMeText = language === 'swedish' ? (
+    <div>
+      <p>
+        Hej där! Jag heter Tim Lindborg, och jag gillar att hålla det intressant. På dagarna är jag en entusiastisk programmerare som spenderar min lediga tid med att fördjupa mig i programmeringsvärlden. Men när arbetsdagen är över är det dags för äventyr i den verkliga världen.
+      </p>
+    </div>
+  ) : (
+    <div>
+      <p>
+        Hey there! I'm Tim Lindborg, and I like to keep things interesting. By day, I'm a coding enthusiast, spending my free time diving into the world of programming. But when work is done, it's time for some down-to-earth adventures.
+      </p>
+    </div>
+  );
+
+  const outdoorAdventuresText = language === 'swedish' ? (
+    <div>
+      <p>
+        Jag älskar att hålla mig aktiv, och en av mina favoritsätt att göra det är att ge mig ut i backarna för en löprunda. Det är något magiskt med de slingrande stigarna som får mig att vilja återvända gång på gång. Det handlar inte om att slå rekord; det handlar om den enkla glädjen att röra på sig och vara omgiven av naturen.
+      </p>
+    </div>
+  ) : (
+    <div>
+      <p>
+        I'm a big fan of staying active, and one of my favorite ways to do that is by hitting the hills for a good run. There's something magical about those winding trails that keeps me coming back for more. It's not about breaking records; it's about the simple joy of moving and being surrounded by nature.
+      </p>
+    </div>
+  );
+
+  const musicalInterestsText = language === 'swedish' ? (
+    <div>
+      <p>
+        Och sedan finns min pålitliga gitarr. När jag inte kodar eller springer, så spelar jag på gitarren bara för skojs skull. Det är mitt sätt att koppla av och låta kreativiteten flöda.
+      </p>
+    </div>
+  ) : (
+    <div>
+      <p>
+        And then there's my trusty guitar. When I'm not coding or running, I'm strumming away just for fun. It's my way of unwinding and letting creativity flow.
+      </p>
+    </div>
+  );
+
+  const lovedOnesText = language === 'swedish' ? (
+    <div>
+      <p>
+        Självklart har jag en fantastisk medbrottsling, min flickvän [Flickvänens namn]. Vi delar dessa passioner och tillbringar kvalitetstid tillsammans, vare sig det är att utforska nya stigar, spela musik eller bara umgås.
+      </p>
+      <p>
+        Men det är inte allt; jag har turen att ha en krets av nära vänner som är som familj. Jag har mina fem bästa vänner, en syster, hennes man, deras bedårande dotter och, självklart, min alltid stöttande mamma. De lägger till ännu mer färg och glädje i mitt liv, och tillsammans skapar vi minnen som varar livet ut.
+      </p>
+    </div>
+  ) : (
+    <div>
+      <p>
+        Of course, I've got a fantastic partner in crime, my girlfriend [Girlfriend's Name]. We share these passions and spend quality time together, whether it's exploring new trails, making music, or just hanging out.
+      </p>
+      <p>
+        But that's not all; I'm lucky to have a circle of close friends who are like family. I've got my five best friends, a sister, her husband, their adorable daughter, and, of course, my ever-supportive mother. They add even more color and joy to my life, and together, we create memories that last a lifetime.
+      </p>
+    </div>
+  );
+
   return (
     <div>
-    <header>
-      <h1 className='hline'>
-        {language === 'swedish' ? 'Om Mig' : 'About Me'}
-      </h1>
-    </header>
-    <div className="about-me-container">
-      <section>
-        <h2 className={`headline-page ${language === 'swedish' ? 'slide-me' : 'slide-from-right fade-in-1'}`}>
-          {language === 'swedish' ? 'Introduktion' : 'Introduction'}
-        </h2>
-        <p className={`fade-in-fast ${language === 'swedish' ? 'slide-me' : 'slide-from-right fade-in-1'}`}>
-          {language === 'swedish'
-            ? 'Hej där! Jag heter Tim Lindborg, och jag gillar att hålla det intressant. På dagarna är jag en entusiastisk programmerare som spenderar min lediga tid med att fördjupa mig i programmeringsvärlden. Men när arbetsdagen är över är det dags för äventyr i den verkliga världen.'
-            : "Hey there! I'm Tim Lindborg, and I like to keep things interesting. By day, I'm a coding enthusiast, spending my free time diving into the world of programming. But when work is done, it's time for some down-to-earth adventures."}
-        </p>
-      </section>
-      <section>
-        <h2 className={`headline-page ${language === 'swedish' ? 'slide-me' : 'slide-from-right fade-in-2'}`}>
-          {language === 'swedish' ? 'Äventyr utomhus' : 'Outdoor Adventures'}
-        </h2>
-        <p className={`fade-in-fast ${language === 'swedish' ? 'slide-me' : 'slide-from-right fade-in-2'}`}>
-          {language === 'swedish'
-            ? 'Jag älskar att hålla mig aktiv, och en av mina favoritsätt att göra det är att ge mig ut i backarna för en löprunda. Det är något magiskt med de slingrande stigarna som får mig att vilja återvända gång på gång. Det handlar inte om att slå rekord; det handlar om den enkla glädjen att röra på sig och vara omgiven av naturen.'
-            : "I'm a big fan of staying active, and one of my favorite ways to do that is by hitting the hills for a good run. There's something magical about those winding trails that keeps me coming back for more. It's not about breaking records; it's about the simple joy of moving and being surrounded by nature."}
-        </p>
-      </section>
-      <section>
-        <h2 className={`headline-page ${language === 'swedish' ? 'slide-me' : 'slide-from-right fade-in-3'}`}>
-          {language === 'swedish' ? 'Musikaliska intressen' : 'Musical Interests'}
-        </h2>
-        <p className={`fade-in-fast ${language === 'swedish' ? 'slide-me' : 'slide-from-right fade-in-3'}`}>
-          {language === 'swedish'
-            ? 'Och sedan finns min pålitliga gitarr. När jag inte kodar eller springer, så spelar jag på gitarren bara för skojs skull. Det är mitt sätt att koppla av och låta kreativiteten flöda.'
-            : "And then there's my trusty guitar. When I'm not coding or running, I'm strumming away just for fun. It's my way of unwinding and letting creativity flow."}
-        </p>
-      </section>
-      <section>
-        <h2 className={`headline-page ${language === 'swedish' ? 'slide-me' : 'slide-from-right fade-in-4'}`}>
-          {language === 'swedish' ? 'Mina nära och kära' : 'My Loved Ones'}
-        </h2>
-        <p className={`fade-in-fast ${language === 'swedish' ? 'slide-me' : 'slide-from-right fade-in-4'}`}>
-        {language === 'swedish'
-        ? "Självklart har jag en fantastisk medbrottsling, min flickvän [Flickvänens namn]. Vi delar dessa passioner och tillbringar kvalitetstid tillsammans, vare sig det är att utforska nya stigar, spela musik eller bara umgås."
-        : "Of course, I've got a fantastic partner in crime, my girlfriend [Girlfriend's Name]. We share these passions and spend quality time together, whether it's exploring new trails, making music, or just hanging out."}
-       </p>
-
-        <p>
-          {language === 'swedish'
-            ? "Men det är inte allt; jag har turen att ha en krets av nära vänner som är som familj. Jag har mina fem bästa vänner, en syster, hennes man, deras bedårande dotter och, självklart, min alltid stöttande mamma. De lägger till ännu mer färg och glädje i mitt liv, och tillsammans skapar vi minnen som varar livet ut."
-            : "But that's not all; I'm lucky to have a circle of close friends who are like family. I've got my five best friends, a sister, her husband, their adorable daughter, and, of course, my ever-supportive mother. They add even more color and joy to my life, and together, we create memories that last a lifetime."}
-        </p>
-      </section>
+      <header>
+        <h1 className="hline">
+          {language === 'swedish' ? 'Om Mig' : 'About Me'}
+        </h1>
+      </header>
+      <div className="about-me-container">
+        <section>
+          <h2 className={`headline-page ${language === 'swedish' ? 'slide-me' : 'slide-from-right fade-in-1'}`}>
+            {language === 'swedish' ? 'Introduktion' : 'Introduction'}
+          </h2>
+          {aboutMeText}
+        </section>
+        <section>
+          <h2 className={`headline-page ${language === 'swedish' ? 'slide-me' : 'slide-from-right fade-in-2'}`}>
+            {language === 'swedish' ? 'Äventyr utomhus' : 'Outdoor Adventures'}
+          </h2>
+          {outdoorAdventuresText}
+        </section>
+        <section>
+          <h2 className={`headline-page ${language === 'swedish' ? 'slide-me' : 'slide-from-right fade-in-3'}`}>
+            {language === 'swedish' ? 'Musikaliska intressen' : 'Musical Interests'}
+          </h2>
+          {musicalInterestsText}
+        </section>
+        <section>
+          <h2 className={`headline-page ${language === 'swedish' ? 'slide-me' : 'slide-from-right fade-in-4'}`}>
+            {language === 'swedish' ? 'Mina nära och kära' : 'My Loved Ones'}
+          </h2>
+          {lovedOnesText}
+        </section>
+      </div>
     </div>
-  </div>
-);
+  );
 };
 
 export default AboutMe;
--- src/pages/Cv.tsx
+++ src/pages/Cv.tsx
@@ -2,48 +2,154 @@ import React from 'react';
 import { useLanguageContext } from '../components/LanguageProvider';
 import lexicon from '../pages/img/logo-lexicon.jpg'; // Replace with the actual path to your first image
 import volvo from '../pages/img/volvo.png'; // Replace with the actual path to your second image
+import '../components/css/NavigationMenu.css'; // Add CSS styling for the CV
 
 export const Cv = () => {
   const { language } = useLanguageContext();
 
+  const careerChangeText = language === 'swedish' ? (
+    <div>
+      <p>
+        På Volvo Braås CE har jag varit i 4 år och svetsat, samtidigt som jag har haft programmering som en hobby på min fritid. Där har jag fått arbeta i team för att tillverka korgar till maskinerna, särskilt flaket till dumptrarna. Det är ett mycket krävande arbete eftersom tillverkningen av korgarna tar betydligt längre tid än alla andra komponenter i hela processen att färdigställa en dumper (maskin). 
+      </p>
+      <p>
+        Nu strävar jag efter att byta karriär eftersom jag önskar en vardag där jag ständigt utvecklas och lär mig nya saker. Det kan vara utmanande att arbeta inom en industri där man producerar samma produkt om och om igen, vilket är varför jag tror att programmering passar mig bättre.
+      </p>
+    </div>
+  ) : (
+    <div>
+      <p>
+        At Volvo Braås CE, I have spent 4 years welding while pursuing programming as a hobby during my free time. During this period, I have worked in teams to manufacture baskets for the machines, specifically the dumpers. This job is particularly demanding as building the baskets takes much longer than any other component in the entire process of completing a dumper (machine).
+      </p>
+      <p>
+        I now aspire to change my career path because I seek a daily routine where I am constantly evolving and learning new things. It can be challenging to work in an industry where you are producing the same product repeatedly, which is why I believe programming is a better fit for me.
+      </p>
+    </div>
+  );
+  
+  // Use the 'careerChangeText' variable wherever needed in your code
+  
+  
+  // Use the 'careerChangeText' variable wherever needed in your code
+  
+
+  // Define the lengthy text separately for better readability
+  const lengthyText =
+  language === 'swedish' ? (
+    <div>
+      <p>
+        Jag avslutade en intensiv tvåårig yrkesutbildning vid Lexicon, en ansedd yrkeshögskola. Denna utbildning var mycket krävande och inkluderade veckovisa avslutande prov, där endast de som klarade proven fick fortsätta sina studier.
+      </p>
+      <p>
+        Under min tid på Lexicon förvärvade jag en djup förståelse för olika ämnen, inklusive MySQL, MVC-arkitektur och React. Mot slutet av programmet samarbetade vi som ett team i ett praktikprojekt för en riktig klient, Milagros Corner. Vi utvecklade framgångsrikt ett 'Point of Sale' (POS)-system åt dem.
+      </p>
+      <p>
+        Efter att ha framgångsrikt avslutat min yrkesutbildning beslutade jag att temporärt arbeta som svetsare samtidigt som jag aktivt förföljde mitt långsiktiga mål att bli programmerare. Idag känner jag mig väl förberedd och entusiastisk inför att inleda min karriär som programmerare. Min hängivenhet och beslutsamhet är orubbliga, och jag ser fram emot att bidra med mina färdigheter till den dynamiska programmeringsbranschen.
+      </p>
+    </div>
+  ) : (
+    <div>
+      <p>
+        I completed an intensive two-year vocational program at Lexicon, a renowned vocational college. This program was highly demanding, featuring weekly final exams, with only those who passed being eligible to continue their studies.
+      </p>
+      <p>
+        During my time at Lexicon, I acquired a profound understanding of various subjects, including MySQL, MVC architecture, and React. Towards the conclusion of the program, we collaborated as a team on an internship project for a real client, Milagros Corner. We successfully developed a 'Point of Sale' (POS) system for them.
+      </p>
+      <p>
+        Upon the successful completion of my vocational training, I decided to temporarily work as a welder while actively pursuing my long-term goal of becoming a programmer. Today, I am well-prepared and enthusiastic about launching my career as a programmer. My dedication and determination are unwavering, and I am eager to contribute my skills to the dynamic field of programming.
+      </p>
+    </div>
+  );
+
   return (
     <div>
-      <header>
-        <h1 className='hline'>{language === 'swedish' ? 'CV' : 'CV'}</h1>
+          <header>
+        <h1 className="hline1" style={{ paddingLeft: '45px' }}>{language === 'swedish' ? 'CV' : 'Resume'}</h1>
       </header>
+
+    <div className="cv">
       <div className="cv-container">
-        <div className="text-cv">
+        <div className="cv-entry">
           <div className="image-placeholder">
-            <img src={volvo} alt="volvo Logo" className='img-class' />
+            <img src={volvo} alt="Volvo Logo" className="cv-image" />
           </div>
-          <strong className="slide-from-right fade-in-1">
+          <strong className="cv-job-title">
             {language === 'swedish' ? 'Volvo Construction Equipment' : 'Volvo Construction Equipment'}
           </strong>
           <br />
-          <em>{language === 'swedish' ? 'Korgsvetsare' : 'Basket Welder'}</em>
-          {language === 'swedish' ? ' 2021 - Nutid' : ' 2021 - Present'}
           <br />
-          {language === 'swedish' ? 'MAG-svetsningsteknik för tunga maskinkomponenter.' : 'Expertise in MAG welding technology for heavy machinery components.'}
-          <br />
+          <p>
+            {careerChangeText}
+          </p>
         </div>
-        <div className="text-cv1">
+        <div className="cv-entry">
           <div className="image-placeholder">
-            <img src={lexicon} alt="lexicon" className="img-class" />
+            <img src={lexicon} alt="Lexicon Logo" className="cv-image" />
           </div>
-          <strong className="slide-from-right fade-in-2">
+          <strong className="cv-job-title">
             {language === 'swedish' ? 'Melagos Corner med Lexicon' : 'Melagos Corner With Lexicon'}
           </strong>
           <br />
-          <em>{language === 'swedish' ? 'Praktik' : 'Internship'}</em>
-          {language === 'swedish' ? ' 2020' : ' 2020'}
           <br />
-          {language === 'swedish' ? 'Utvecklade ett försäljningssystem med användning av MySQL, MVC-arkitektur och React.' : 'Developed a sales system using MySQL, MVC architecture, and React.'}
-          <br />
+          {/* Use the variable for the lengthy text */}
+          <p>{lengthyText}</p>
         </div>
       </div>
     </div>
+    </div>
   );
 };
+
+
+
+
+
+// import React from 'react';
+// import { useLanguageContext } from '../components/LanguageProvider';
+// import lexicon from '../pages/img/logo-lexicon.jpg'; // Replace with the actual path to your first image
+// import volvo from '../pages/img/volvo.png'; // Replace with the actual path to your second image
+
+// export const Cv = () => {
+//   const { language } = useLanguageContext();
+
+//   return (
+//     <div>
+//       <header>
+//         <h1 className='hline'>{language === 'swedish' ? 'CV' : 'CV'}</h1>
+//       </header>
+//       <div className="cv-container">
+//         <div className="text-cv">
+//           <div className="image-placeholder">
+//             <img src={volvo} alt="volvo Logo" className='img-class' />
+//           </div>
+//           <strong className="slide-from-right fade-in-1">
+//             {language === 'swedish' ? 'Volvo Construction Equipment' : 'Volvo Construction Equipment'}
+//           </strong>
+//           <br />
+//           <em>{language === 'swedish' ? 'Korgsvetsare' : 'Basket Welder'}</em>
+//           {language === 'swedish' ? ' 2021 - Nutid' : ' 2021 - Present'}
+//           <br />
+//           {language === 'swedish' ? 'MAG-svetsningsteknik för tunga maskinkomponenter.' : 'Expertise in MAG welding technology for heavy machinery components.'}
+//           <br />
+//         </div>
+//         <div className="text-cv1">
+//           <div className="image-placeholder">
+//             <img src={lexicon} alt="lexicon" className="img-class" />
+//           </div>
+//           <strong className="slide-from-right fade-in-2">
+//             {language === 'swedish' ? 'Melagos Corner med Lexicon' : 'Melagos Corner With Lexicon'}
+//           </strong>
+//           <br />
+//           <em>{language === 'swedish' ? 'Praktik' : 'Internship'}</em>
+//           {language === 'swedish' ? ' 2020' : ' 2020'}
+//           <br />
+//           {language === 'swedish' ? 'Utvecklade ett försäljningssystem med användning av MySQL, MVC-arkitektur och React.' : 'Developed a sales system using MySQL, MVC architecture, and React.'}
+//           <br />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
 
 
