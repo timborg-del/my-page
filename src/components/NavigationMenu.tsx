1e9f164831612d83835842ea806de0f71fd5385b
timborg-del <timl@live.se>
2024-03-28 12:40:07 +0100

    hamburgermenu

--- src/components/NavigationMenu.tsx
+++ src/components/NavigationMenu.tsx
@@ -1,38 +1,41 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import './css/NavigationMenu.css';
 import { useLanguageContext } from '../components/LanguageProvider';
- 
-
-  
-
+import './css/NavigationMenu.css';
 
 const NavigationMenu: React.FC = () => {
   const { language } = useLanguageContext();
-return (
-   
-    <nav className="navbar">
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <nav className={`navbar ${menuOpen ? 'active' : ''}`}>
+      <button className="menu-button" onClick={toggleMenu}>
+        <span className="menu-icon">&#9776;</span> {/* Hamburger menu icon */}
+      </button>
       <ul className="navbar-list">
-      <li className="navbar-item">
-        <Link to=""><span className="icofont">&#xef47;</span>{language === 'swedish' ? ' Hem' : ' Home'}</Link>
+        <li className="navbar-item">
+          <Link to=""><span className="icofont">&#xef47;</span>{language === 'swedish' ? ' Hem' : ' Home'}</Link>
         </li>
         <li className="navbar-item">
-        <Link to="/cv"><span className="icofont">&#xe82f;</span>{language === 'swedish' ? ' CV' : ' Resume'}</Link>
+          <Link to="/cv"><span className="icofont">&#xe82f;</span>{language === 'swedish' ? ' CV' : ' Resume'}</Link>
         </li>
         <li className="navbar-item">
-        <Link to="/aboutme"><span className="icofont">&#xed52;</span>{language === 'swedish' ? ' Om Mig' : ' About Me'}</Link>
+          <Link to="/aboutme"><span className="icofont">&#xed52;</span>{language === 'swedish' ? ' Om Mig' : ' About Me'}</Link>
         </li>
         <li className="navbar-item">
-        <Link to="/portfolio"><span className="icofont">&#xe967;</span>{language === 'swedish' ? ' Mina Projekt' : ' My Projects'}</Link>
+          <Link to="/portfolio"><span className="icofont">&#xe967;</span>{language === 'swedish' ? ' Mina Projekt' : ' My Projects'}</Link>
         </li>
         <li className="navbar-item">
-        <Link to="/contact"><span className="icofont">&#xec50;</span>{language === 'swedish' ? ' Kontakt' : ' Contact'}</Link>
+          <Link to="/contact"><span className="icofont">&#xec50;</span>{language === 'swedish' ? ' Kontakt' : ' Contact'}</Link>
         </li>
       </ul>
-
-      </nav>
-    
+    </nav>
   );
 };
 
 export default NavigationMenu;
+
